a0b901e1ee299b27d2b26225ac4345ebce0c9eb1
Tiberius Silivestru <tsilivestru@conversantmedia.com>
2017-01-31 08:41:29 -0600

    changed port, changed a font size

--- api/deploy.sh
+++ api/deploy.sh
@@ -5,7 +5,7 @@ docker rm socket-api && \
 docker rmi socket-api && \
 docker build -t socket-api . && \
 docker run --name socket-api \
-  -p 9111:8080 \
+  -p 11111:8080 \
   -v ~/Development/projects/websocket-poc/api:/usr/src/app \
   -e "NODE_ENV=dev" \
   -d socket-api run dev
