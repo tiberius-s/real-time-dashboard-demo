476aecf8d806ceeb0f97717b6157c9205ccf9c3f
Tiberius Silivestru <tsilivestru@conversantmedia.com>
2017-01-31 10:10:42 -0600

    set the sockt server via env variable

--- api/deploy.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-sudo docker kill demo-socket-api && \
-sudo docker rm demo-socket-api && \
-sudo docker rmi demo-socket-api && \
-sudo docker build -t demo-socket-api . && \
-sudo docker run --name demo-socket-api \
-  -p 10001:8080 \
-  -d demo-socket-api
